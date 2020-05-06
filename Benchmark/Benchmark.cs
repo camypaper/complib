af048ffaa7e70a9bff9bee825cd73c47cb65fb86
camypaper <camypaper@vivaldi.net>
2020-05-06 16:37:09 +0900

    Modify Benchmark

--- Benchmark/Benchmark.cs
+++ Benchmark/Benchmark.cs
@@ -1,6 +1,6 @@
 using BenchmarkDotNet.Running;
 
-namespace Complib.Benchmark {
+namespace CompLib.Benchmark {
 	public class Benchmark {
 		static void Main(string[] args) => BenchmarkSwitcher.FromAssembly(typeof(Benchmark).Assembly).Run(args);
 	}
